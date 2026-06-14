a5b3f333124bbffa34cd0a91bb724bf5453b7cf6
AfiaaSattar <afiasattar2@gmail.com>
2026-06-14 22:04:03 +0300

    14th June

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,7 +1,14 @@
+import personalPic from '../assets/react.svg';
 export default function ProfileCard(){
     return(
         <div className="profile-card">
-            helllo
+
+           <div className="profile-header">
+            <img src={personalPic} className="profile-img"/>
+            <span className="spa"></span>
+           </div>
+
+
         </div>
     );
 }
