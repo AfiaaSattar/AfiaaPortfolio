52a2e2592207aab05234ec0a5e53fc041d6b183c
AfiaaSattar <afiasattar2@gmail.com>
2026-06-15 01:23:26 +0300

    15 June

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,13 +1,15 @@
 import personalPic from '../assets/react.svg';
+
 export default function ProfileCard(){
     return(
         <div className="profile-card">
 
            <div className="profile-header">
-            <img src={personalPic} className="profile-img"/>
-            <span className="spa"></span>
+            <div className="avatar-wrapper">
+                <img src={personalPic} alt="Afiaa" className="profile-img"/>
+                <span className="status-dot"></span>
+            </div>
            </div>
-
 
         </div>
     );
