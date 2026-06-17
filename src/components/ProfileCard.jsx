b48e389798a7410404715c56a59fdf3f9faa7dc2
AfiaaSattar <afiasattar2@gmail.com>
2026-06-17 23:00:43 +0300

    17th June

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,4 +1,6 @@
 import personalPic from '../assets/react.svg';
+import styled from 'styled-components';
+
 
 const SKILLS_DATA = [
   { id: 1, name: 'React', isHot: false },
@@ -9,22 +11,27 @@ const SKILLS_DATA = [
   { id: 6, name: 'REST API', isHot: true },
 ];
 
-function SkillRows({name, isHot}) => {
-    return(
-        SKILLS_DATA.map()
-    )
-}
+const SkillTag = styled.div`
+display: inline-flex;
+gap: 15px;
+margin: 10px;
+border: 1px solid red;
+padding: 15px 10px;
+border-radius: 20px;
 
+
+${props => props.$isHot &&`
+    border: 1px solid green;
+    `}
+`
 export default function ProfileCard(){
     return(
         <div className="profile-card">
-           <div className="profile-header">
-
+           <div className="profile-header">  
             <div className="avatar-wrapper">
                 <img src={personalPic} alt="Afiaa" className="profile-img"/>
                 <span className="status-dot"></span>
             </div>
-
             <div>
                 <span className="eyebrow-text">Digital Portfolio</span>
                 <h1 className='main-title'>Hi, I'm Afiaa</h1>
@@ -33,8 +40,11 @@ export default function ProfileCard(){
                 </p>
             </div>
            </div>
-
-
+           {SKILLS_DATA.map(({id,name,isHot}) => (
+            <SkillTag key={id} $isHot={isHot}> 
+             {name}
+            </SkillTag>
+           ))}
         </div>
     );
 }
