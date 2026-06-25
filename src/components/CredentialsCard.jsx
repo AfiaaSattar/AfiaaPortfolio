8cec22473a7f212e1b495264a4036bc2aff8e230
AfiaaSattar <afiasattar2@gmail.com>
2026-06-25 19:06:37 +0300

    25th June

--- src/components/CredentialsCard.jsx
+++ src/components/CredentialsCard.jsx
@@ -1,19 +1,19 @@
 
-import {  FaRegEnvelope } from "react-icons/fa";
+import { FiAward } from "react-icons/fi";
 export default function CredentialsCards(){
     return(
         <div className="credentials-cards">
             <div className="credentials-header">
                 <div className="credentials-left-side">
-                    < FaRegEnvelope />
+                   <FiAward className="fiAwardClass" />
                     <div className="credentials-text-box">
-                        <h4>credentials</h4>
+                        <h3>Credentials</h3>
                         <span>Certifications & Awards</span>
                     </div>
                 </div>   
                <div className="credentials-right-side">
                     <span> 42 total</span>
-                </div>        
+               </div>        
             </div>
         </div>
     );
