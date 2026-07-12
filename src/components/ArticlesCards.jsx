596c3728332515e95eb25c99d5fc3988508ba242
AfiaaSattar <afiasattar2@gmail.com>
2026-07-12 14:14:48 +0300

    12th july 2nd

--- src/components/ArticlesCards.jsx
+++ src/components/ArticlesCards.jsx
@@ -1,7 +1,41 @@
+import { IoMdBook } from "react-icons/io";
 export default function ArticlesCards(){
     return(
         <div className="articles-card">
-            artical
+        {/*articles-header*/}
+            <div className="articles-header">
+                <div className="articles-left-side">
+                  <IoMdBook className="fiAwardClass"/>
+                    <div className="articles-text-box">
+                        <h3>Recent Articles</h3>
+                        <span>Published writing & insights</span>
+                    </div>
+                </div>   
+                <div className="articles-right-side">
+                    <span>3 posts</span>
+                </div>        
+            </div>
+
+        {/*articles-body*/}
+
+        <div className="articles-body"> 
+            <div className="articles-body-header">
+                <span>Medium</span>
+                <span>8 min read</span>
+            </div>
+            <h5>Mastering WordPress Security: A Complete Guide</h5>
+            <p>h ahiuahfuia uihaukhf hkja hkjf h ahfah jkhfa jkhfa
+                kja lkfj lkaj sfklj ksfj klj skafj jj flkajflkjsjf 
+            </p>
+            <div className="articles-body-footer">
+                <span> May 18,2026</span>
+            </div>
+        </div>
+
+        {/*View All Credentials*/}
+            <div className="articles-button">
+               <button>View All Credentials</button>
+            </div>
         </div>
     );
 }
