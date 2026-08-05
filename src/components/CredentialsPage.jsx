213a6e1feef5cf3d5b7cbfb63380b1b173b93bc0
AfiaaSattar <afiasattar2@gmail.com>
2026-08-05 16:44:16 +0300

    Implement credential search

--- src/components/CredentialsPage.jsx
+++ src/components/CredentialsPage.jsx
@@ -1,31 +1,55 @@
 import { Link } from "react-router-dom";
 import { categories, certificates } from "../data/credentials.js";
 import test from "../assets/google.webp";
+import { MdArrowBack } from "react-icons/md";
+import { useState } from "react";
 
 import { MdOutlineOpenInNew } from "react-icons/md";
-
 export default function CredentialsPage() {
-  return (
+    const [searchTerm, setSearchTerm] = useState("");
+     const [selectedCategory, setSelectedCategory] = useState("All");
+
+{/* Search Functionality */}
+    function handleSearchChange(e) {
+        setSearchTerm(e.target.value);
+    }
+    const filteredCertificates = certificates.filter((certificate) => {
+        const matchSearch = certificate.certificateName
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+
+        const matchCategory = 
+        selectedCategory === "" ||
+        selectedCategory === "All" ||
+        certificate.category === selectedCategory;
+        return matchSearch && matchCategory;
+});
+   
+{/* Render the component */}
+return (
     <div style={{ padding: "50px" }}>
       <Link to="/" className="back-to-portfolio">
-        <button> Back to Portfolio</button>
+        <button style={{borderRadius: "10px"}} className="credentials-registry"> 
+            <MdArrowBack />
+             Back to Portfolio
+        </button>
       </Link> 
-      <p>Credentials Registry</p>
-      <h1> Alll Credentials</h1>
+      <p className="credentials-registry">Credentials Registry</p>
+      <h1> All Credentials</h1>
       <p>42 certifications & awards across Web Development, Leadership, and Tech Support</p>
 
 {/* Search and Categories */}
       <div className="credentials-search">
-        <input className="credentials-search" type="text" placeholder="Search Credentials" />
+        <input onChange={handleSearchChange} value={searchTerm} className="credentials-search-input" type="text" placeholder="Search Credentials" />
        
         <div className="credentials-categories">
             {categories.map((category) => (
-                <span
+                <button onClick={() => setSelectedCategory(category)}
                 key={category}
-                className="credentials-category"
+                className={`credentials-category ${selectedCategory === category ? "active" : ""}`}
                 >
                   {category}
-                </span>
+                </button>
             ))}
         </div>     
       </div>
@@ -33,18 +57,19 @@ export default function CredentialsPage() {
      <p>Showing 42 of 42 credentials</p>
             
     <div className="credentials-search-cards"> 
-        {certificates.map((certificate) => (
+        {filteredCertificates.map((certificate) => (
 
-        <div className="credentials-search">
+        <div className="credentials-search"> 
 
             <div className="credentialsontainer">
-            <certificate.icon className="headerTag" style={{ color: "#2295b4c0" }} />        <div className="credentials-details">
+            <certificate.icon className="headerTag" style={{ color: "#2295b4c0" }} />  
+                  <div className="credentials-details">
                     <p>{certificate.companyName}</p>
                     <p>{certificate.date}</p>
                 </div>
             </div>
 
-            <h4>{certificate.certificateName}t</h4>
+            <h4>{certificate.certificateName}</h4>
 
                 <div className="articles-body-footer">
                     <span className="credentials-category" style={{ backgroundColor: "#2294b441", border: "1px solid #2295b4c0", color: "#2295b4c0" }}>
@@ -55,7 +80,6 @@ export default function CredentialsPage() {
                     </a>    
                 </div>
             </div>
-
                 ))}   
      </div>
     </div>
